4c7dabe94976d75fc35e81c0ac97140fe3e44674
Anacamino17 <53191539+Anacamino17@users.noreply.github.com>
2025-04-03 12:12:51 +0200

    Delete Mi portafolio directory

--- Mi portafolio/main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () { 
-    const boton = document.querySelector(".button-toggle"); 
-    const menu = document.querySelector(".menu"); 
-
-    boton.addEventListener("click", function () { 
-        menu.classList.toggle("open"); 
-    });
-});
